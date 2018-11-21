8e068cee13d84d9a2349b1d25d0f8a7ae3dbfd30
Your Name <you@example.com>
2018-11-21 09:53:32 +0800

    关于我提交修改

---
+++ admin/src/components/About.vue
@@ -0,0 +1,94 @@
+<template>
+    <div style="height:100%">
+        <head-nav></head-nav>
+        <Aside></Aside>
+        <div class="list-container main">
+            <h2>
+                文章列表 /
+                <span>ARTICLE LIST</span>
+            </h2>
+            <hr>
+            <div class="content">
+                <textarea></textarea>
+            </div>
+            <section class="btn-container">
+                <button class="not-del" @click="updateAbout">提交修改</button>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+import HeadNav from "@/components/common/HeadNav";
+import Aside from "@/components/common/Aside";
+import 'simplemde/dist/simplemde.min.css'
+import SimpleMDE from 'simplemde'
+import request from '../utils/request'
+export default {
+  name: "About",
+  data() {
+    return {
+        simplemde:''/*编辑器*/
+    };
+  },
+  components: {
+    HeadNav,
+    Aside
+  },
+  mounted(){
+      //页面加载完毕之后发送请求
+      this.simplemde = new SimpleMDE({
+            autoDownloadFontAwesome: false,
+            spellChecker: false
+      })
+    request({
+      url:'/articles/about',
+      method:'post'
+    }).then(res=>{
+      this.simplemde.value(res[0].content)
+    }).catch(err=>{
+      console.log(err)
+    })
+  },
+  methods:{
+      updateAbout(){
+      request({
+      url: '/articles/updata/about',
+        method: 'post',
+        data:{
+           content:this.simplemde.value()
+        }
+       }).then(res=>{
+         console.log(res)
+      }).catch(err=>{
+        console.log(err)
+      })
+      }
+  }
+};
+</script>
+
+<style type="text/scss" lang="scss" scoped>
+/*记得引入全局变量的文件*/
+@import "../assets/style/variable";
+main {
+  @include flex($justify: space-between);
+  height: calc(100% - 48px);
+  padding: 0.5em 0;
+}
+.article-list {
+  width: 40%;
+  height: 100%;
+  overflow: auto;
+  padding: 0 0.8em 0.5em 0;
+  .list {
+    padding: 0;
+    list-style: none;
+  }
+
+}
+.content{
+    font-size:24px;
+  }
+</style>
+
---
+++ admin/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+//引入登录组件
+import Login from '@/components/Login'
+//引入主页面组件
+import List from '@/components/List'
+//引入关于我
+import About from '@/components/About'
+export default new Router({
+  routes: [
+    {
+      path:'/login',
+      component:Login
+    },
+    {
+      path:'/list',
+      component:List
+    },
+    {
+      path:'/about',
+      component:About
+    },
+    {path:'*',redirect:'/login'}
+  ]
+})
